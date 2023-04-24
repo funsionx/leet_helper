7d82956bb0629d7f29905933823ebfe09d9ee823
Maxim Agapov <agapovxd@yandex.ru>
2023-04-24 21:34:44 +0300

    +layout (early version)

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,14 +1,20 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     colors: {
-      // Using modern `rgb`
       color_bg: "rgb(var(--color-bg))",
       color_button1: "rgb(var(--color-button_first))",
-      color_button2: "rgb(var(--color-button_second))"
+      color_button2: "rgb(var(--color-button_second))",
+      white: "rgb(255, 255, 255)",
+      dark_blue: "rgb(52, 48, 69)"
     },
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Montserrat", ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 };
